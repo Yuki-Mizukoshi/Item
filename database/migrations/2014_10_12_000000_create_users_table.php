388183aeae7eafb48b1cd85a0862bb4441d42f82
Yuki-Mizukoshi <120461676+Yuki-Mizukoshi@users.noreply.github.com>
2023-03-27 19:03:02 +0900

    2023-03-26

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->integer('role');
-            $table->string('email')->unique();
+            $table->string('email')->charaset("utf8")->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
